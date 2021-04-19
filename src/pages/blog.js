cbf16914748d4751b525d68f1cc889d6c490abbd
Branden <blacour@hgsdigital.com>
2021-04-18 21:58:23 -0500

    added recommended articles, and category page. added tons of logic for recommendations

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby";
 import get from "lodash/get";
 import { Helmet } from "react-helmet";
 import styles from "./blog.module.css";
+import { Container, Row, Col } from "react-bootstrap";
 import Layout from "../components/layout";
 import ArticlePreview from "../components/article-preview";
 
@@ -10,12 +11,54 @@ class BlogIndex extends React.Component {
   render() {
     const siteTitle = get(this, "props.data.site.siteMetadata.title");
     const posts = get(this, "props.data.allContentfulBlogPost.edges");
-
+    const categoriesNoDupe = [];
+    const categories = get(
+      this,
+      "props.data.allContentfulCategories.edges"
+    ).map(({ node }) => {
+      if (node.category !== null && node.category !== undefined) {
+        return node.category;
+      }
+    });
+    categories.forEach((cat) => {
+      if (!categoriesNoDupe.includes(cat) && cat !== undefined) {
+        categoriesNoDupe.push(cat);
+      }
+    });
+    console.log(categoriesNoDupe);
     return (
       <Layout location={this.props.location}>
         <div style={{ background: "#fff" }}>
           <Helmet title={siteTitle} />
-          <div className={styles.hero}>Blog</div>
+          <div className="blog-title">Categories</div>
+          <div className="container main-container">
+            <Container className="preview-container">
+              {categoriesNoDupe.map((cat, i) => {
+                if (cat !== null && i <= categoriesNoDupe.length / 2 - 1) {
+                  return (
+                    <Row className="justify-content-around">
+                      <Col className="category" lg={8}>
+                        <h2>{cat}</h2>
+                      </Col>
+                    </Row>
+                  );
+                }
+              })}
+            </Container>
+            <Container className="">
+              {categoriesNoDupe.map((cat, i) => {
+                if (cat !== null && i > categoriesNoDupe.length / 2 - 1) {
+                  return (
+                    <Row className="justify-content-around">
+                      <Col className="category" lg={8}>
+                        <h2>{cat}</h2>
+                      </Col>
+                    </Row>
+                  );
+                }
+              })}
+            </Container>
+          </div>
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
             <ul className="article-list">
@@ -57,6 +100,13 @@ export const pageQuery = graphql`
               html
             }
           }
+        }
+      }
+    }
+    allContentfulCategories(filter: {}) {
+      edges {
+        node {
+          category
         }
       }
     }
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,21 +5,33 @@ import get from "lodash/get";
 import Img from "gatsby-image";
 import Layout from "../components/layout";
 import { Container, Row, Col } from "react-bootstrap";
-
+import ArticlePreview from "../components/article-preview";
 import heroStyles from "../components/hero.module.css";
 import blogStyles from "./blog-post.module.css";
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = get(this.props, "data.contentfulBlogPost");
+    const posts = get(this, "props.data.allContentfulBlogPost.edges");
     const siteTitle = get(this.props, "data.site.siteMetadata.title");
     const iframe = post.youtubeVideo ? post.youtubeVideo.link : null;
+    const postCategory =
+      post.category !== null ? post.category[0].category : null;
+    const matchedCategories = posts.filter(({ node }) => {
+      if (
+        node.category !== null &&
+        postCategory === node.category[0].category &&
+        node.title !== post.title
+      ) {
+        return node;
+      }
+    });
     return (
       <Layout location={this.props.location}>
         <div style={{ background: "#fff" }}>
           <Helmet title={`${post.title} | ${siteTitle}`} />
           <Container className={blogStyles.hero} fluid>
-            <h1 className="section-headline">{post.title}</h1>
+            <h1 className="section-headline mt-4">{post.title}</h1>
           </Container>
           <Container fluid>
             <Row className="justify-content-center">
@@ -52,6 +64,39 @@ class BlogPostTemplate extends React.Component {
               dangerouslySetInnerHTML={{ __html: iframe }}
             ></div>
           ) : null}
+          <div className=""></div>
+
+          <div className="main-titles-container">
+            <h2 className="section-headline">Related Blog Posts</h2>
+            <h2 className="section-headline">Main Feed</h2>
+          </div>
+          <div className="container main-container">
+            <Container className="preview-container">
+              {matchedCategories.map(({ node }) => {
+                return (
+                  <Row key={node.slug}>
+                    <Col lg={12}>
+                      <ArticlePreview article={node} />
+                    </Col>
+                  </Row>
+                );
+              })}
+            </Container>
+
+            <Container className="preview-container">
+              {posts.map(({ node }) => {
+                if (node.title !== post.title) {
+                  return (
+                    <Row key={node.slug}>
+                      <Col lg={12}>
+                        <ArticlePreview article={node} />
+                      </Col>
+                    </Row>
+                  );
+                } else return null;
+              })}
+            </Container>
+          </div>
         </div>
       </Layout>
     );
@@ -64,18 +109,54 @@ export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
+
       publishDate(formatString: "MMMM Do, YYYY")
       heroImage {
         fluid(maxWidth: 1500, background: "rgb:000000") {
           ...GatsbyContentfulFluid_tracedSVG
         }
       }
+      category {
+        category
+      }
       youtubeVideo {
         link
       }
+
       body {
         childMarkdownRemark {
           html
+        }
+      }
+    }
+    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+      edges {
+        node {
+          title
+          slug
+          publishDate(formatString: "MMMM Do, YYYY")
+          tags
+
+          popular
+          category {
+            category
+          }
+          author {
+            ... on ContentfulAuthor {
+              id
+              name
+            }
+          }
+          heroImage {
+            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+          description {
+            childMarkdownRemark {
+              html
+            }
+          }
         }
       }
     }
